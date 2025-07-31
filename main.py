c99efb67aaf361690e116288f5e949f5c44f88a2
YXTang03 <2244656809@qq.com>
2025-07-31 10:00:18 +0800

    cmd available

---
+++ main.py
@@ -0,0 +1,39 @@
+from sshtunnel import SSHTunnelForwarder
+import psycopg2
+from .utils import calculate
+
+
+def main(
+        ssh_address:str, 
+        ssh_port:int, 
+        ssh_username:str, 
+        ssh_password:str, 
+        remote_address:str, 
+        remote_port:int, 
+        database_name:str, 
+        db_user:str, 
+        db_password:str, 
+        table:str, 
+        field:str,        
+        db_host:str = '127.0.0.1', 
+        batch_size = 1000
+):
+    server = SSHTunnelForwarder(
+    ssh_address_or_host=(ssh_address, ssh_port),
+    ssh_username=ssh_username, 
+    ssh_password = ssh_password, 
+ 
+    
+    remote_bind_address=(remote_address , remote_port)) 
+    server.start()
+    conn = psycopg2.connect(database = database_name, 
+                                user = db_user,     
+                                password = db_password, 
+                                host = db_host,   
+                                port = server.local_bind_port)
+    calculate(
+        con = conn, 
+        table = table, 
+        field = field,
+        batch_size = batch_size
+    )
